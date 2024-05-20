f18c16c4ee9f912c731201ebe9c72761b35e81f8
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-20 14:48:53 -0400

    feat: add more dynamic values

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -4,16 +4,22 @@ import './App.css'
 function App() {
   const title = "Welcome to the new blog";
   const likes = 50;
-
+  //const person = {name:"michael", job:"worker", age:"27", color:"white", funny:"yes"};
+  const link = "https://www.google.com";
   return (
     <>
       <div>
         <div className="content">
           <h1>{title}</h1>
           <p>Liked {likes} times</p>
+          {/* <p>{person}</p> */}
+          <p>{10}</p>
+          <p>{"hello"}</p>
+          <p>{[1,2,3,4,5]}</p>
+          <p>{Math.random() * 10}</p>
+          <a href={link}>Google</a>
         </div>
       </div>
-      
     </>
   )
 }
