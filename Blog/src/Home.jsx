bc133dda8e31f7847e79ed861068198e839ba1d5
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-06-13 15:26:45 -0400

    feat add loading message

--- Blog/src/Home.jsx
+++ Blog/src/Home.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then(res => {
@@ -10,10 +10,12 @@ const Home = () => {
             })
             .then((data) => {
                 setBlogs(data);
+                setIsPending(false);
             })
     }, []);
     return (
         <div className="home">
+            {isPending && <div> loading... </div>}
             {blogs && <Bloglist blogs={blogs} title="All blogs"/>}
         </div>
       );
