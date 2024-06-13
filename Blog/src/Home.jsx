519741a5f416cfebe06395e747b6d5cccf0e49c7
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-06-13 13:07:24 -0400

    feat: delete button

--- Blog/src/Home.jsx
+++ Blog/src/Home.jsx
@@ -2,10 +2,7 @@ import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id != id);
-        setBlogs(newBlogs);
-    }
+    
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then(res => {
@@ -17,7 +14,7 @@ const Home = () => {
     }, []);
     return (
         <div className="home">
-            {blogs && <Bloglist blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
+            {blogs && <Bloglist blogs={blogs} title="All blogs"/>}
         </div>
       );
 }
